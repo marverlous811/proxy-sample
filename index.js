972d0a4b00721425ba809ae6f81898178100a412
shadowWalker <hieu811@gmail.com>
2024-11-24 23:52:46 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const http = require('http')
 const port = parseInt(process.env.PORT || '8080')
 
+const SEC = 1000
+const MINUTE = 60 * SEC
+const HOUR = 60 * MINUTE
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 app.post('/webhook', (req, res) => {
   console.log(req.body)
-  res.send({ status: 'ok', data: { ...req.body } })
+  res.send({ status: 'ok' })
 })
 
-app.get('/ping', (req, res) => {
+app.post('/metrics', (req, res) => {
+  console.log(req.body.ts)
+  res.send({ status: 'ok' })
+})
+
+app.get('/ping', async (req, res) => {
   res.set('Connection', 'close')
   res.send({ status: 'pong' })
 })
